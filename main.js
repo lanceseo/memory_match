b580b7d90a109d8e3b1fe4a0377dea7f8e8a4a99
lanceseo <lance9980@gmail.com>
2016-03-26 16:23:16 -0700

    card shuffling function added #6

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
 
 	});
 	$("#options").on("click", function() {
-		playCards.DOM();
+		playCards.shuffleCards(18);
+		playCards.DOM(18);
 	});
 
 });
@@ -13,19 +14,38 @@ $(document).ready(function(){
 
 var Cards = function() {
 	var self = this;
-	var cardImages = ["images/ff_Ballade.jpg","images/ff_CutMan.jpg","images/ff_Enker.jpg","images/ff_GutsMan.jpg",
-    "images/ff_Punk.jpg","images/ff_QuickMan.jpg","images/ff_Quint.jpg","images/ff_Shadowman.jpg","images/ff_Skullman.jpg"];
+	var cardImages = ["images/mm_001.jpg", "images/mm_002.jpg", "images/mm_003.jpg", "images/mm_004.jpg", "images/mm_005.jpg",
+						 "images/mm_006.jpg", "images/mm_007.jpg", "images/mm_008.jpg", "images/mm_009.jpg"];
+	var totalCards = [];
 	
-	self.shuffleCards = function() {
+	self.shuffleCards = function(level) {
+		var concatCount = (level / cardImages.length);
+		for (var i=0; i<concatCount; i++) {
+			totalCards = totalCards.concat(cardImages);
+		}
 
+		var curr_index = totalCards.length;
+        var temp_value, rand_index;
+        while (0 !== curr_index) {
+            rand_index = Math.floor(Math.random() * curr_index);
+            curr_index -= 1;
+            temp_value = totalCards[curr_index];
+            totalCards[curr_index] = totalCards[rand_index];
+            totalCards[rand_index] = temp_value;
+        }
 	};
 
-	self.DOM = function() {
-		for (var i=0; i<cardImages.length; i++) {
-			var cardFront = $("<img>").attr("src", cardImages[i]);
+	self.DOM = function(level) {
+		for (var i=0; i<totalCards.length; i++) {
+			var cardFront = $("<img>").attr("src", totalCards[i]);
 			$("#game-area").append(cardFront);
 		}
 	};
+};
+
+var Stats = function() {
+	var self = this;
+
 };
 
 
