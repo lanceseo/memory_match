91b8157cc4d8bc2d9c839eadac66c5bbddd7287b
lanceseo <lance9980@gmail.com>
2016-03-28 19:08:20 -0700

    re-organize OOP methods, shuffle fix #1 #6

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
 
 	});
 	$("#options").on("click", function() {
-		playCards.shuffleCards(18);
+		playCards.totalCards(18);
+		playCards.shuffleCards();
 		playCards.DOM(18);
 	});
 
@@ -18,28 +19,31 @@ var Cards = function() {
 	var self = this;
 	var cardImages = ["images/mm_001.jpg", "images/mm_002.jpg", "images/mm_003.jpg", "images/mm_004.jpg", "images/mm_005.jpg",
 						 "images/mm_006.jpg", "images/mm_007.jpg", "images/mm_008.jpg", "images/mm_009.jpg"];
-	var totalCards = [];
 	
-	self.shuffleCards = function(level) {
+	self.totalCards = function(level) {
 		var concatCount = (level / cardImages.length);
+		var allCards = [];
 		for (var i=0; i<concatCount; i++) {
-			totalCards = totalCards.concat(cardImages);
+			allCards = allCards.concat(cardImages);
 		}
-
-		var curr_index = totalCards.length;
+		self.totalCards = allCards;
+	};
+	
+	self.shuffleCards = function() {
+		var curr_index = self.totalCards.length;
         var temp_value, rand_index;
         while (0 !== curr_index) {
             rand_index = Math.floor(Math.random() * curr_index);
             curr_index -= 1;
-            temp_value = totalCards[curr_index];
-            totalCards[curr_index] = totalCards[rand_index];
-            totalCards[rand_index] = temp_value;
+            temp_value = self.totalCards[curr_index];
+            self.totalCards[curr_index] = self.totalCards[rand_index];
+            self.totalCards[rand_index] = temp_value;
         }
 	};
 
 	self.DOM = function(level) {
-		for (var i=0; i<totalCards.length; i++) {
-			var cardFront = $("<img>").attr("src", totalCards[i]);
+		for (var i=0; i<self.totalCards.length; i++) {
+			var cardFront = $("<img>").attr("src", self.totalCards[i]);
 			var divFront = $("<div class='front'>").append(cardFront);
 			var cardBack = $("<img>").attr("src", "images/mm_back.jpg");
 			var divBack = $("<div class='back'>").append(cardBack);
@@ -59,6 +63,24 @@ var Cards = function() {
 		};
 		showFront();
 		showBack();
+	};
+	var firstCard = false;
+	var secondCard = false;
+
+	self.checkFirstSecond = function(cardClicked) {
+		if (firstCard === false && secondCard === false) {
+			firstCard = true;
+		}
+	};
+
+	self.checkMatch = function(card1, card2) {
+		var matchFlag = false;
+		if (card1 === card2) {
+			matchFlag = true;
+		} else {
+			matchFlag = false;
+		}
+		return matchFlag;
 	};
 
 };
