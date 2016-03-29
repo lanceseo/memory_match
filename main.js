7aa5cd65a6ffcd63097ece8c95b328f3498a5837
lanceseo <lance9980@gmail.com>
2016-03-28 21:00:35 -0700

    flip handler fixed to use array #3

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
 	$("#game-area").on("click", ".card", function(){
 		//run an OOP function
 		playCards.flip(this);
-		console.log(this);
 
 	});
 	$("#options").on("click", function() {
-		playCards.totalCards(18);
+		playCards.getTotalCards(18);
 		playCards.shuffleCards();
 		playCards.DOM(18);
 	});
@@ -17,70 +16,67 @@ $(document).ready(function(){
 
 var Cards = function() {
 	var self = this;
-	var cardImages = ["images/mm_001.jpg", "images/mm_002.jpg", "images/mm_003.jpg", "images/mm_004.jpg", "images/mm_005.jpg",
+	var frontImages = ["images/mm_001.jpg", "images/mm_002.jpg", "images/mm_003.jpg", "images/mm_004.jpg", "images/mm_005.jpg",
 						 "images/mm_006.jpg", "images/mm_007.jpg", "images/mm_008.jpg", "images/mm_009.jpg"];
+	var backImage = "images/mm_back.jpg";
+	var totalCards = [];
 	
-	self.totalCards = function(level) {
-		var concatCount = (level / cardImages.length);
+	self.getTotalCards = function(level) {
+		var concatCount = (level / frontImages.length);
 		var allCards = [];
 		for (var i=0; i<concatCount; i++) {
-			allCards = allCards.concat(cardImages);
+			allCards = allCards.concat(frontImages);
 		}
-		self.totalCards = allCards;
+		totalCards = allCards;
 	};
 	
 	self.shuffleCards = function() {
-		var curr_index = self.totalCards.length;
+		var curr_index = totalCards.length;
         var temp_value, rand_index;
         while (0 !== curr_index) {
             rand_index = Math.floor(Math.random() * curr_index);
             curr_index -= 1;
-            temp_value = self.totalCards[curr_index];
-            self.totalCards[curr_index] = self.totalCards[rand_index];
-            self.totalCards[rand_index] = temp_value;
+            temp_value = totalCards[curr_index];
+            totalCards[curr_index] = totalCards[rand_index];
+            totalCards[rand_index] = temp_value;
         }
 	};
 
 	self.DOM = function(level) {
-		for (var i=0; i<self.totalCards.length; i++) {
-			var cardFront = $("<img>").attr("src", self.totalCards[i]);
+		for (var i=0; i<totalCards.length; i++) {
+			var cardFront = $("<img>").attr("src", totalCards[i]);
 			var divFront = $("<div class='front'>").append(cardFront);
-			var cardBack = $("<img>").attr("src", "images/mm_back.jpg");
+			var cardBack = $("<img>").attr("src", backImage);
 			var divBack = $("<div class='back'>").append(cardBack);
             var divCard = $("<div class='card'>").append(divFront, divBack);
 			$("#game-area").append(divCard);
 		}
 	};
 
+	self.matchFlag = false;
+	self.matchCounter = 0;
+	var flippedCards = [];
+
 	self.flip = function(cardClicked) {
-		var showFront = function() {
-        $(cardClicked).find(".back").hide();
-		};
-		var showBack = function() {
-			setTimeout(function() {
-        		$(cardClicked).find(".back").show();
-		    }, 2000);
-		};
-		showFront();
-		showBack();
-	};
-	var firstCard = false;
-	var secondCard = false;
-
-	self.checkFirstSecond = function(cardClicked) {
-		if (firstCard === false && secondCard === false) {
-			firstCard = true;
+		var cardImg1, cardImg2 = null;
+		if ($(cardClicked).find(".back").is(':visible')) {
+	        $(cardClicked).find(".back").hide();
+	        flippedCards.push(cardClicked);	        	        
 		}
-	};
-
-	self.checkMatch = function(card1, card2) {
-		var matchFlag = false;
-		if (card1 === card2) {
-			matchFlag = true;
-		} else {
-			matchFlag = false;
-		}
-		return matchFlag;
+		if (flippedCards.length === 2) {
+			cardImg1 = $(flippedCards[0]).find(".front img").attr("src");
+			cardImg2 = $(flippedCards[1]).find(".front img").attr("src");
+			if (cardImg1 === cardImg2) {
+				//matchFlag = true;
+				self.matchCounter += 1;
+				flippedCards = [];
+				console.log(self.matchCounter);
+			} else {
+				$(flippedCards[0]).find(".back").show();
+				$(flippedCards[1]).find(".back").show();
+				flippedCards = [];
+			}
+		}        
 	};
 
 };
