d46a699f8c88dccfa0325818029a831fc45ae81d
lanceseo <lance9980@gmail.com>
2016-04-04 15:53:09 -0700

    OOP redo based on TicTacToe OOP...

--- main.js
+++ main.js
@@ -1,20 +1,47 @@
 
+var aGame = null;
 $(document).ready(function(){
-	$("#game-area").on("click", ".card", function(){
-		aGame.cardClick(this);
-	});
-	$("#options").on("click", ".btn", function() {
-		var option = $(this).attr("id");
-		console.log("type: " + typeof option);
-		aGame.optionClick(option);
-	});
+	// $("#game-area").on("click", ".card", function(){
+	// 	aGame.cardClick(this);
+	// });
+	// $("#options").on("click", ".btn", function() {
+	// });
+	aGame = new GameTemplate($("#game-area"));
+	aGame.getTotalCards(18);
+	aGame.createCards();
 });
 
 
-var GameController = function() {
+var GameTemplate = function(mainElement) {
+	var self = this;
 	var frontImgs = ["images/mm_001.jpg", "images/mm_002.jpg", "images/mm_003.jpg", "images/mm_004.jpg", "images/mm_005.jpg",
 				"images/mm_006.jpg", "images/mm_007.jpg", "images/mm_008.jpg", "images/mm_009.jpg"];
 	var backImg = "images/mm_back.jpg";
+	
+	this.element = mainElement;
+	this.cardArray = [];
+	var totalCards = null;
+
+	this.totalImages = function(numCards) {
+		var concatCount = (numCards / frontImages.length);
+		var allCards = [];
+		for (var i=0; i<concatCount; i++) {
+			allCards = allCards.concat(frontImages);
+		}
+		totalCards = allCards;
+	};
+	this.shuffleImages = function() {
+
+	}
+
+	this.createCards = function(numCards) {
+		for (var i=0; i<=numCards; i++) {
+			var card = new CardTemplate();
+			var cardElement = card.createSelf();
+			this.cardArray.push(card);
+			this.element.append(cardElement);
+		}
+	}
 
 	this.cardClick = function(theCard) {
 
@@ -26,32 +53,38 @@ var GameController = function() {
 			this.resetCards();
 		}
 	}
-	this.createCards = function(option) {
-		for (var i=0; i<frontImgs.length; i++) {
-
-		}
-	}
 	this.resetCards = function() {
 
 	}
 };
 
-var Cards = function() {
+var CardTemplate = function() {
+	this.element = null;
 
+	// this.createSelf = function() {
+	// 	var cardFront = $("<img>").attr("src", frontImg);
+	// 	var divFront = $("<div class='front'>").append(cardFront);
+	// 	var cardBack = $("<img>").attr("src", backImg);
+	// 	var divBack = $("<div class='back'>").append(cardBack);
+	//     var divCard = $("<div class='card'>").append(divFront, divBack);
+	// 	$("#game-area").append(divCard);
+	// }
 
-};
-
-var Card = function(frontImg, backImg) {
-	this.imgName = frontImg;
-	this.backFace = true;
-	this.createDOM = function() {
+//how to attach images, when to append to HTML output DOM, how to check match, how to check win
+	this.createSelf = function(frontImg, backImg) {
 		var cardFront = $("<img>").attr("src", frontImg);
 		var divFront = $("<div class='front'>").append(cardFront);
 		var cardBack = $("<img>").attr("src", backImg);
 		var divBack = $("<div class='back'>").append(cardBack);
 	    var divCard = $("<div class='card'>").append(divFront, divBack);
-		$("#game-area").append(divCard);
+    	this.element = divCard.click(this.cardClick);
+    	return this.element;
 	}
+	this.cardClick = function() {
+
+	}
+
+
 };
 
 var aGame = new GameController();
